ad7dfa360662916ac3d49d5cffd43a1c0a6d7639
kuromore@kuromore.com <kuromore@kuromore.com>
2018-12-06 14:03:50 +0900

    Validated

--- demoProject/src/main/java/com/example/demo/HelloController.java
+++ demoProject/src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,34 @@
 package com.example.demo;
 
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
 		return "Hello Spring Boot!";
+	}
+
+	@RequestMapping("/parameter/param1/{param1}")
+	public String ValidatedTest(
+			@PathVariable(required = true)
+			@NotBlank
+			String param1,
+
+			@RequestParam
+			@NotBlank
+			String param2
+			) {
+
+		return "param1:" + param1 + " param2:" + param2;
 	}
 
 }
